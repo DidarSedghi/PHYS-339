3ec2eeb4123ebf1846536e810ae31df924629fbf
Didar Sedghi <144862501+DidarSedghi@users.noreply.github.com>
2025-02-06 16:49:38 -0500

    Add files via upload
    better python codes for analysis.

--- analysis_4_LOWERmean.py
+++ analysis_4_LOWERmean.py
@@ -4,45 +4,18 @@ from scipy.optimize import curve_fit
 from scipy.special import factorial
 from scipy.stats import poisson
 
-"""
-Appendix
-"""
-# # Read the content of the data files and put in string_data.
-# for i in range(len(paths)):
-#     with open(paths[i], "r") as doc:
-#         content = doc.read()
-#         globals()[f'string_data{i+1}'] = content.split(',')
-#         print(f"String data {i+1}", globals()[f'string_data{i+1}'], '\n')
-
-# # Transform the string values into integer values.
-# for index in range(len(paths)):
-#     globals()[f'data{index + 1}'].append(int(globals()[f'string_data{index + 1}']))
-#     print(globals()[f'data{index + 1}'])
-
-# # Declare path of all the data files.
-# path1 = '/Users/didarsedghi/Desktop/Work/McGill/U4 (2024-2025)/U4 Winter/PHYS-339 Measurmenets and Lab/Data/Data February 5/LOWER MEAN/lab3_data_CPI_LOWER_200s_(178_5mm)_sample1_interval1s_2025-02-05-15h09m32s.txt'
-# path2 = '/Users/didarsedghi/Desktop/Work/McGill/U4 (2024-2025)/U4 Winter/PHYS-339 Measurmenets and Lab/Data/Data February 5/LOWER MEAN/lab3_data_CPI_LOWER_200s_(178_5mm)_sample2_interval1s_2025-02-05-14h31m32s.txt'
-# path3 = '/Users/didarsedghi/Desktop/Work/McGill/U4 (2024-2025)/U4 Winter/PHYS-339 Measurmenets and Lab/Data/Data February 5/LOWER MEAN/lab3_data_CPI_LOWER_200s_(178_5mm)_sample3_interval1s_2025-02-05-15h19m22s.txt'
-# path4 = '/Users/didarsedghi/Desktop/Work/McGill/U4 (2024-2025)/U4 Winter/PHYS-339 Measurmenets and Lab/Data/Data February 5/LOWER MEAN/lab3_data_CPI_LOWER_200s_(178_5mm)_sample4_interval1s_2025-02-05-14h48m55s.txt'
-# path5 = '/Users/didarsedghi/Desktop/Work/McGill/U4 (2024-2025)/U4 Winter/PHYS-339 Measurmenets and Lab/Data/Data February 5/LOWER MEAN/lab3_data_CPI_LOWER_200s_(178_5mm)_sample5_interval1s_2025-02-05-14h58m45s.txt'
-
-# for i in range(1, 6):
-#     with open(globals()[f'path{i}'], "r") as doc:
-#         content = doc.read()
-#         globals()[f'string_data{i}'] = content.split(',')
-#         print(globals()[f'string_data{i}'])
 
 """
 Part I: Logistics and data extraction.
 """
 # Declare path of all the data files.
 paths = [
-    '/Users/didarsedghi/Desktop/Work/McGill/U4 (2024-2025)/U4 Winter/PHYS-339 Measurmenets and Lab/Data/Data February 5/LOWER MEAN/lab3_data_CPI_LOWER_200s_(178_5mm)_sample1_interval1s_2025-02-05-15h09m32s.txt',
-    '/Users/didarsedghi/Desktop/Work/McGill/U4 (2024-2025)/U4 Winter/PHYS-339 Measurmenets and Lab/Data/Data February 5/LOWER MEAN/lab3_data_CPI_LOWER_200s_(178_5mm)_sample2_interval1s_2025-02-05-14h31m32s.txt',
-    '/Users/didarsedghi/Desktop/Work/McGill/U4 (2024-2025)/U4 Winter/PHYS-339 Measurmenets and Lab/Data/Data February 5/LOWER MEAN/lab3_data_CPI_LOWER_200s_(178_5mm)_sample3_interval1s_2025-02-05-15h19m22s.txt',
-    '/Users/didarsedghi/Desktop/Work/McGill/U4 (2024-2025)/U4 Winter/PHYS-339 Measurmenets and Lab/Data/Data February 5/LOWER MEAN/lab3_data_CPI_LOWER_200s_(178_5mm)_sample4_interval1s_2025-02-05-14h48m55s.txt',
-    '/Users/didarsedghi/Desktop/Work/McGill/U4 (2024-2025)/U4 Winter/PHYS-339 Measurmenets and Lab/Data/Data February 5/LOWER MEAN/lab3_data_CPI_LOWER_200s_(178_5mm)_sample5_interval1s_2025-02-05-14h58m45s.txt'
-]
+    r"P:\lab3_data_CPI_LOWER_200s_(178_5mm)_sample1_interval1s_2025-02-05-15h09m32s.txt",
+    r"P:\lab3_data_CPI_LOWER_200s_(178_5mm)_sample2_interval1s_2025-02-05-14h31m32s.txt",
+    r"P:\lab3_data_CPI_LOWER_200s_(178_5mm)_sample3_interval1s_2025-02-05-15h19m22s.txt",
+    r"P:\lab3_data_CPI_LOWER_200s_(178_5mm)_sample4_interval1s_2025-02-05-14h48m55s.txt",
+    r"P:\lab3_data_CPI_LOWER_200s_(178_5mm)_sample5_interval1s_2025-02-05-14h58m45s.txt"
+    ]
 
 
 # Read the content of the data files and put in string_data dictionary.
@@ -51,7 +24,7 @@ for i in range(len(paths)):
     with open(paths[i], "r") as doc:
         content = doc.read()
         string_data[i] = content.split(',')
-        #print(f"String data {i+1}: ", string_data[i])
+        print(f"String data {i+1}: ", string_data[i])
 # All the data is now put in a dictionary as lists with string values.
 
 
@@ -59,13 +32,15 @@ for i in range(len(paths)):
 data = {}
 for index in range(len(string_data)):
     if string_data[index][-1] == '':
-        string_data[index].pop()
+        string_data[index].pop() # Deletes that pesky ''.
         
     for j in range(len(string_data[index])):
         string_data[index][j] = int(string_data[index][j])
     data[index] = string_data[index]
 
-#print(data)
+print("Data dictionnary: ", data)
+for i in range(len(data)):
+    print("Data ", i + 1, data[i])
 
 
 """
@@ -79,13 +54,56 @@ for x in range(len(data)):
 #print(bins_matrix[0])
 
 # Creating histograms.
+def Poisson_fit(k, lamb):
+    return poisson.pmf(k, lamb) * sum(counts)
+
 for j in range(len(bins_matrix)):
-    counts, 
+    counts, bins_matrix[j], patches = plt.hist(data[j],
+                                               bins=bins_matrix[j],
+                                               edgecolor="black",
+                                               alpha=0.7, label=f'Mean = {round(np.mean(data[j]), 2)}')
+    poisson_lambda = np.mean(data[j])
+    bin_values_new = np.delete(bins_matrix[j], -1)
+    parameters, cov_matrix = curve_fit(Poisson_fit, bin_values_new, counts, p0=[poisson_lambda])
+    print("Mean: ", j, parameters[0])
+    print("Covariance matrix: ", j, cov_matrix)
+    
+    
+    plt.plot(bin_values_new, Poisson_fit(bin_values_new, *parameters), marker='o',
+             linestyle='-', label='Poisson fit')
+    
+    print("COUNTS: ", j+1, counts)
+    plt.legend()
+    plt.style.use('seaborn-v0_8-paper')
+    plt.title(f'Lower Mean Sample {j+1} (178.5mm)')
+    plt.xlabel('Clicks per Interval')
+    plt.ylabel('Occurrences')
+    plt.show()
+
+
+"""
+Part III: Poisson fitting each replica sample data set.
+"""
+def Poisson_fit(k, lamb, factor):
+    return factor * poisson.pmf(k, lamb)
 
 
 
+"""
+Part IV: Plot the total collapsed 
+"""
 
 
+data_set = np.concatenate(list(data.values()))
+print(len(data_set), data_set)
+
+bin_data_set_values = np.arange(min(data_set), max(data_set) + 2, 1)
+
+new_counts, bin_data_set_values, patches = plt.hist(data_set, bins=bin_data_set_values,
+                                                    edgecolor='black', alpha=0.7,
+                                                    label='Massive data')
+plt.legend()
+plt.show()
 
 
 
--- analysis_5_LOWERmean.py
+++ analysis_5_LOWERmean.py
@@ -7,7 +7,9 @@ from scipy.stats import poisson
 This code only handles a specific data sample set for reference. The entire
 sample is taken care of in analysis 4.
 """
-
+"""
+NEWER.
+"""
 
 
 
@@ -16,7 +18,8 @@ sample is taken care of in analysis 4.
 Part I: Data extraction and logistics.
 """
 # Declare path of data.
-path = '/Users/didarsedghi/Desktop/Work/McGill/U4 (2024-2025)/U4 Winter/PHYS-339 Measurmenets and Lab/Data/Data February 5/LOWER MEAN/lab3_data_CPI_LOWER_200s_(178_5mm)_sample1_interval1s_2025-02-05-15h09m32s.txt'
+path = r"\\campus.mcgill.ca\students\users\dsedgh\Desktop\lab3_data_CPI_LOWER_200s_(178_5mm)_sample1_interval1s_2025-02-05-15h09m32s.txt"
+
 
 # Open data file and put into a string form in an array.
 with open(path, "r") as doc:
@@ -33,66 +36,41 @@ print("Data in integer data form: ", data)
 
 
 
-
-
-
-
-
-
 """
 Part II: Plotting and graphing.
 """
 
 bin_values = np.arange(min(data), max(data) + 2, 1)
-print("BIN VALUES: ", bin_values)
-
-bin_centers = (bin_values[:-1] + bin_values[1:]) / 2
-print("BIN CENTERS: ", bin_centers)
-
-center = []
-for j in range(len(bin_centers)):
-    center.append(int(bin_centers[j]))
-print("CENTER: ", np.array(center))
-    
-
-counts, bin_values, patches = plt.hist(data, bins=bin_values,
-                              edgecolor="black", alpha=0.7,label='Data')
-plt.legend()
-plt.show()
+#print("BIN VALUES: ", bin_values)
 
 
-counts, bin_values, patches = plt.hist(data, bins=bin_values,
-                              edgecolor="black", alpha=0.7,label='Data Centered')
-
-plt.xticks(bin_centers)
-plt.xticks(bin_centers, labels=[f'{int(x)}' for x in bin_centers])
-plt.title('centered')
-plt.legend()
+counts, bin_values, patches = plt.hist(data, bins=bin_values, edgecolor='black',
+                                       alpha=0.7, label='Data')
 #plt.show()
+print("Summation of counts and counts: ", sum(counts), counts)
 
+print("BIN VALUES: ", len(bin_values), bin_values, "\n")
+print("COUNTS: ", len(counts), counts, "\n")
 
-"""
-Part III: Poisson Fit
-"""
+bin_values_new = np.delete(bin_values, -1)
 
-# Define the Poisson function for fitting
 def Poisson_fit(k, lamb):
-    return poisson.pmf(k, lamb) * sum(counts)  # Scale to match total counts
+    return poisson.pmf(k, lamb) * sum(counts)
 
-# Estimate initial lambda (mean of data)
 poisson_lambda = np.mean(data)
 
-# Fit the Poisson model to histogram data using bin_centers
-parameters, cov_matrix = curve_fit(Poisson_fit, bin_centers, counts, p0=[poisson_lambda])
+parameters, cov_matrix = curve_fit(Poisson_fit, bin_values_new, counts, p0=[poisson_lambda])
 
+print("Optimized poisson mean: ", parameters[0])
+print("Covariance matrix: ", cov_matrix)
 
+plt.plot(bin_values_new, Poisson_fit(bin_values_new, *parameters), marker='o',
+         linestyle='', label='Poisson fit')
 
-# Plot the fitted Poisson distribution
-plt.plot(bin_centers, Poisson_fit(bin_centers, *parameters), linestyle='', marker='o', label='Fitted Poisson')
-
-plt.xticks(bin_centers, labels=[f'{int(x)}' for x in bin_centers])
-plt.title('Poisson Fit to Data')
 plt.legend()
+plt.xlabel('Clicks Per Interval')
+plt.ylabel('Occurrences')
+plt.title('Lower Mean Sample 1 (178.5mm)')
 plt.show()
 
 
